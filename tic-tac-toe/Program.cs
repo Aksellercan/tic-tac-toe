8ab3544aca2f6c704726f671495d848c66647cc0
Aksel 144482 <pzx114044@student.wsb.poznan.pl>
2022-12-20 23:26:37 +0100

    Add project files.

---
+++ tic-tac-toe/Program.cs
@@ -0,0 +1,194 @@
+﻿namespace TIC_TAC_TOE
+{
+    class Program
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int flag = 0;
+        static void Main(string[] args)
+        {
+            Menu();
+
+            void Menu()
+            {
+                Console.Clear();
+                Console.WriteLine("1. New Game: ");
+                Console.WriteLine("2. About Author");
+                Console.WriteLine("3. Exit");
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    tictactoe();
+                }
+                else if (input == 2)
+                {
+                    aboutdev();
+                    Console.WriteLine("To go back to main menu press the Enter key");
+                    string o = Console.ReadLine();
+                    if (o == "")
+                    {
+                        Menu();
+                    }
+
+                }
+                else if (input == 3)
+                {
+                    Console.WriteLine("Are you sure you want to quit? y/n");
+                    string choice = Console.ReadLine();
+                    if (choice == "y")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else if (choice == "n")
+                    {
+                        Menu();
+                    }
+                }
+
+            }
+
+
+            void tictactoe()
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Player 1:X and Player 2:O");
+                    Console.WriteLine("\n");
+                    if (player % 2 == 0)
+                    {
+                        Console.WriteLine("Player 2 turn");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1 turn");
+                    }
+                    Console.WriteLine("\n");
+                    Board();
+                    choice = int.Parse(Console.ReadLine());
+
+                    if (arr[choice] != 'X' && arr[choice] != 'O')
+                    {
+                        if (player % 2 == 0)
+                        {
+                            arr[choice] = 'O';
+                            player++;
+                        }
+                        else
+                        {
+                            arr[choice] = 'X';
+                            player++;
+                        }
+                    }
+                    else
+
+                    {
+                        Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Please wait 2 second board is loading again.....");
+                        Thread.Sleep(2000);
+                    }
+                    flag = CheckWin();
+                }
+
+
+                while (flag != 1 && flag != -1);
+
+                Console.Clear();
+                Board();
+                if (flag == 1)
+
+                {
+                    Console.WriteLine("Player {0} has won", (player % 2) + 1);
+                    Console.WriteLine("Press Enter key to go back to Main Menu");
+                    Console.ReadLine();
+                    Menu();
+                }
+                else
+                {
+                    Console.WriteLine("Draw");
+                    Console.WriteLine("Press Enter key to go back to Main Menu");
+                    Console.ReadLine();
+                    Menu();
+                }
+                Console.ReadLine();
+            }
+
+            void aboutdev()
+            {
+                Console.WriteLine("Made by Aksel Ercan");
+            }
+
+        }
+
+        private static void Board()
+        {
+            Console.WriteLine($" {arr[1]} | {arr[2]} | {arr[3]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {arr[4]} | {arr[5]} | {arr[6]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {arr[7]} | {arr[8]} | {arr[9]} ");
+        }
+
+        private static int CheckWin()
+        {
+            #region Horzontal Winning Condtion
+
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            #endregion
+            #region vertical Winning Condtion
+
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            #endregion
+            #region Diagonal Winning Condition
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            #endregion
+            #region Checking For Draw
+
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            #endregion
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
